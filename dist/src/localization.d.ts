cf097249e27121dfe920fa37cbf1d8accd070830
dim <turovtsovd@rocketdata.io>
2021-08-09 21:38:27 +0300

    refactor

--- dist/src/localization.d.ts
+++ dist/src/localization.d.ts
@@ -1,2 +1,2 @@
-import { LocalizationConfigType, LocalizationLibraryType } from './../library';
+import { LocalizationConfigType, LocalizationLibraryType } from '../library';
 export declare function createLocalization<TranslationKeys extends string, LocaleName extends string>(localizationConfig: LocalizationConfigType<TranslationKeys, LocaleName>): LocalizationLibraryType<TranslationKeys, LocaleName>;
