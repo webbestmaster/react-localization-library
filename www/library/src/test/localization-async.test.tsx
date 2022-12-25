1e1c69cca8fa0f158d7ee249056790e2bba85850
dim <web.best.master@gmail.com>
2022-12-25 12:46:57 +0100

    update

--- www/library/src/test/localization-async.test.tsx
+++ www/library/src/test/localization-async.test.tsx
@@ -192,7 +192,7 @@ describe('Localization async', () => {
             </LocalizationProvider>
         );
 
-        await waitFor(() => expect(testingLocaleName).toEqual('ru-RU'), {interval: 50, timeout: 300});
+        await waitFor(() => expect(testingLocaleName).toEqual('ru-RU'), {interval: 50, timeout: 1000});
 
         unmount();
     });
